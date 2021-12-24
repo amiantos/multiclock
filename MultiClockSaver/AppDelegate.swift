39873c097d77a24f6e4359be9c035e413f274f3b
Brad Root <bradroot@me.com>
2021-12-24 01:53:15 -0800

    add license notices to all files

--- MultiClockSaver/AppDelegate.swift
+++ MultiClockSaver/AppDelegate.swift
@@ -4,7 +4,9 @@
 //
 //  Created by Brad Root on 12/21/21.
 //
-
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Cocoa
 
