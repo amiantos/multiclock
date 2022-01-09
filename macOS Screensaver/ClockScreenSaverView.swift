1cc04675a7bbdcbc54077fb75bcc390a5c73ef9b
Brad Root <bradroot@me.com>
2022-01-08 19:07:36 -0800

    turn off debug logging in screensaver

--- macOS Screensaver/ClockScreenSaverView.swift
+++ macOS Screensaver/ClockScreenSaverView.swift
@@ -21,7 +21,7 @@ final class ClockScreenSaverView: ScreenSaverView {
     lazy var sheetController: ConfigureSheetController = ConfigureSheetController()
 
     override init?(frame: NSRect, isPreview: Bool) {
-        Log.logLevel = .debug
+        Log.logLevel = .off
         Log.debug("Launching...")
         super.init(frame: frame, isPreview: isPreview)
         animationTimeInterval = 1.0
